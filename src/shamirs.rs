ed109981cce6f194cf7a7024fac0ea6175875061
Euan Deas <33450935+euandeas@users.noreply.github.com>
2024-02-26 15:35:42 +0000

    feat: Switch to implementation that produces smaller shares

--- src/shamirs.rs
+++ src/shamirs.rs
@@ -1,11 +1,102 @@
 use crate::{gf256::GF256, random::random_no_zero_distinct_set};
-// TODO: Probably ideal to use Result<T, E> instead of panicking
 
-pub type BuildShares = fn(&[u8], usize, usize) -> Result<Vec<Vec<u8>>, &'static str>;
-pub type RebuildSecret = fn(Vec<Vec<u8>>) -> Result<Vec<u8>, &'static str>;
-
-// TODO: For each part we must make sure that the x values are unique
 pub fn build_shares(secret: &[u8], k: usize, n: usize) -> Result<Vec<Vec<u8>>, &'static str> {
+    assert!(
+        k <= n,
+        "Threshold should be less than or equal to the number of shares."
+    );
+
+    // create polynomial for each byte
+    let polys: Vec<Vec<GF256>> = secret
+        .iter()
+        .enumerate()
+        .map(|(_, byte)| {
+            let mut row: Vec<GF256> = vec![GF256::ZERO; k];
+            row[0] = GF256::from(*byte);
+
+            let random_bytes = random_no_zero_distinct_set(k - 1);
+            for i in 1..k {
+                row[i] = GF256::from(random_bytes[i - 1]);
+            }
+
+            row
+        })
+        .collect();
+
+    let mut shares: Vec<Vec<u8>> = vec![vec![0u8; 0]; n];
+
+    let random_bytes = random_no_zero_distinct_set(n);
+    for (i, share) in shares.iter_mut().enumerate() {
+        share.push(random_bytes[i]);
+
+        for poly in polys.iter() {
+            let mut eval: GF256 = GF256::ZERO;
+            let mut power: GF256 = GF256::ONE;
+            for coeff in poly.iter() {
+                eval += *coeff * power;
+                power *= GF256::from(random_bytes[i]);
+            }
+
+            share.push(eval.as_u8());
+        }
+    }
+
+    Ok(shares)
+}
+
+pub fn rebuild_secret(shares: Vec<Vec<u8>>) -> Result<Vec<u8>, &'static str> {
+    // TODO: Check For Valid Shares
+
+    // Lagrange Interpolation:
+    // For Each Part of the Secret
+    // x = shares[0]
+    // y = shares[0..]
+    let mut secret = vec![0u8; shares[0].len() - 1];
+
+    for i in 1..shares[0].len() {
+        let mut secret_temp = GF256::ZERO;
+        for share in shares.iter() {
+            let mut num = GF256::ONE;
+            let mut denom = GF256::ONE;
+            for share2 in shares.iter() {
+                if share[0] == share2[0] {
+                    continue;
+                };
+
+                num *= GF256::from(share2[0]);
+                denom *= GF256::from(share2[0]) - GF256::from(share[0]);
+            }
+
+            secret_temp += GF256::from(share[i]) * num * denom.mul_inv();
+        }
+
+        secret[i - 1] = secret_temp.as_u8();
+    }
+
+    Ok(secret)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_shamirs() {
+        for _ in 0..1000 {
+            assert_eq!(
+                "Hello! Testing!".as_bytes().to_vec(),
+                rebuild_secret(build_shares("Hello! Testing!".as_bytes(), 3, 5).unwrap()).unwrap()
+            );
+        }
+    }
+}
+/*
+
+ALTERNATIVE IMPLEMENTATION
+This implementation randomises x values for each byte and so it produces shares of double the original secret length.
+This is not needed in the current use case of this library, and unsure if it provides any benefit to just randomising x for each share.
+
+pub fn build_shares_randomised(secret: &[u8], k: usize, n: usize) -> Result<Vec<Vec<u8>>, &'static str> {
     assert!(
         k <= n,
         "Threshold should be less than or equal to the number of shares."
@@ -52,8 +143,7 @@ pub fn build_shares(secret: &[u8], k: usize, n: usize) -> Result<Vec<Vec<u8>>, &
     Ok(shares)
 }
 
-// TODO: Validate this works after changes to creating shares
-pub fn rebuild_secret(shares: Vec<Vec<u8>>) -> Result<Vec<u8>, &'static str> {
+pub fn rebuild_secret_randomised(shares: Vec<Vec<u8>>) -> Result<Vec<u8>, &'static str> {
     // TODO: Check For Valid Shares
 
     // Lagrange Interpolation:
@@ -82,19 +172,4 @@ pub fn rebuild_secret(shares: Vec<Vec<u8>>) -> Result<Vec<u8>, &'static str> {
     }
 
     Ok(secret)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_shamirs_simple() {
-        for _ in 0..1000 {
-            assert_eq!(
-                "Hello! Testing!".as_bytes().to_vec(),
-                rebuild_secret(build_shares("Hello! Testing!".as_bytes(), 3, 5).unwrap()).unwrap()
-            );
-        }
-    }
-}
+} */
