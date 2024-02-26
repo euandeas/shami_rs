fd64426235112051ff4067237cb81ac25dfa2115
Euan Deas <33450935+euandeas@users.noreply.github.com>
2024-02-26 14:24:50 +0000

    feat: fully implemented bip wrapper

--- src/bip_wrapper.rs
+++ src/bip_wrapper.rs
@@ -1,32 +1,18 @@
 use crate::shamirs::{BuildShares, RebuildSecret};
 use bip39::Mnemonic;
 
-
-fn print_bip() {
-    let m = match Mnemonic::parse_normalized("hundred match learn goddess figure filter zone grocery step tuition manual marriage polar spice inquiry") {
-        Ok(m) => m,
-        Err(_) => return,
-    };
-
-    let entropy = &m.to_entropy_array().0[..(((m.word_count() as f64 * 352.0 / 33.0) / 8.0).ceil())];
-
-        println!("{:?}", entropyw
-        );
-}
-
-fn verify_mnemonic(secret: &[u8])  -> Result<Mnemonic, &'static str>{
+fn verify_mnemonic(secret: &[u8]) -> Result<Mnemonic, &'static str> {
     let string = match std::str::from_utf8(secret) {
         Ok(s) => s,
-        Err(_) => return Err("Invalid String")
+        Err(_) => return Err("Invalid String"),
     };
 
     match Mnemonic::parse_normalized(string) {
         Ok(m) => Ok(m),
-        Err(_) => Err("Invalid Mnemonic")
+        Err(_) => Err("Invalid Mnemonic"),
     }
 }
 
-/* d
 pub fn build_shares_bip(
     secret: &[u8],
     k: usize,
@@ -34,20 +20,48 @@ pub fn build_shares_bip(
     f: BuildShares,
 ) -> Result<Vec<Vec<u8>>, &'static str> {
     let m = verify_mnemonic(secret)?;
-    
-    f(str::trim(m.to_entropy_array().0).as_bytes(), k, n);
+
+    let shares = match f(&m.to_entropy(), k, n) {
+        Ok(shares) => shares,
+        Err(e) => return Err(e),
+    };
+
+    Ok(shares)
 }
 
 pub fn rebuild_secret_bip(shares: Vec<Vec<u8>>, f: RebuildSecret) -> Result<Vec<u8>, &'static str> {
-    unimplemented!()
-} */
+    let secret = match f(shares) {
+        Ok(secret) => secret,
+        Err(e) => return Err(e),
+    };
+
+    let m = match Mnemonic::from_entropy(&secret) {
+        Ok(m) => m,
+        Err(_) => return Err("Invalid Mnemonic"),
+    };
+
+    Ok(m.to_string().into_bytes())
+}
 
 #[cfg(test)]
 mod tests {
+    use crate::shamirs::{build_shares, rebuild_secret};
+
     use super::*;
 
     #[test]
     fn test_bip_simple() {
-        print_bip();
+        const TEST_MNEMONIC: &str = "hundred match learn goddess figure filter zone grocery step tuition manual marriage polar spice inquiry";
+        
+        for _ in 0..1000 {
+            assert_eq!(
+                TEST_MNEMONIC.as_bytes().to_vec(),
+                rebuild_secret_bip(
+                    build_shares_bip(TEST_MNEMONIC.as_bytes(), 3, 5, build_shares).unwrap(),
+                    rebuild_secret
+                )
+                .unwrap()
+            );
+        }
     }
 }
